0e8c5557db28a4dca028d7bfaa957218a52e9914
ameni <gharbiameni17@gmail.com>
2023-09-22 11:28:32 +0100

    init

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('./connect.js');
+
+const heroRoute = require('./routes/hero.js');
+
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//http://127.0.0.1:3000
+
+app.use( '/hero' , heroRoute );
+
+
+app.use( '/getimage' , express.static('./upload')  );
+
+
+app.listen(  
+    3000 
+    ,
+    ()=>{
+        console.log('server work !!!!');
+    }
+    
+);
